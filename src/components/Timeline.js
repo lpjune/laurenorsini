2c53a71bff9a5a8e67cb624a5cda684b2eac9ea7
lpjune <laurenporsini@gmail.com>
2020-08-02 18:23:39 -0500

    timeline work

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,7 +8,7 @@ import "react-vertical-timeline-component/style.min.css";
 
 const Timeline = () => {
     return (
-        <Container>
+        <Container style={{background: "black"}}>
             <Typography variant="h4" component="h4">
                 Experience & Education
             </Typography>
@@ -23,106 +23,67 @@ const Timeline = () => {
                     contentArrowStyle={{
                         borderRight: "7px solid  rgb(33, 150, 243)",
                     }}
-                    date="2011 - present"
+                    date="2018 - present"
                     iconStyle={{
                         background: "rgb(33, 150, 243)",
                         color: "#fff",
                     }}
                 >
                     <h3 className="vertical-timeline-element-title">
-                        Creative Director
+                        Software Engineer
                     </h3>
                     <h4 className="vertical-timeline-element-subtitle">
-                        Miami, FL
+                        Center for Advanced Vehicular Systems
                     </h4>
                     <p>
-                        Creative Direction, User Experience, Visual Design,
-                        Project Management, Team Leading
+                        ▪ Develop webapps with React and Flask to communicate
+                        with Android app ▪ Create Android app in Kotlin for
+                        drone research ▪ Server streams drone commands and live
+                        video between app and webapp
                     </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    date="2010 - 2011"
+                    date="2019"
                     iconStyle={{
                         background: "rgb(33, 150, 243)",
                         color: "#fff",
                     }}
                 >
                     <h3 className="vertical-timeline-element-title">
-                        Art Director
+                        Full-stack Web Intern
                     </h3>
                     <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
+                        C Spire
                     </h4>
                     <p>
-                        Creative Direction, User Experience, Visual Design, SEO,
-                        Online Marketing
+                        ▪ Developed new features and integrated them into
+                        e-commerce prod ▪ JavaScript (Angular, jQuery) and CSS,
+                        Java (Spring) back-end
                     </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
-                    date="2008 - 2010"
+                    date="2018 - 2019"
                     iconStyle={{
                         background: "rgb(33, 150, 243)",
                         color: "#fff",
                     }}
                 >
                     <h3 className="vertical-timeline-element-title">
-                        Web Designer
+                        Teaching Assistant
                     </h3>
                     <h4 className="vertical-timeline-element-subtitle">
-                        Los Angeles, CA
+                        Mississippi State University
                     </h4>
-                    <p>User Experience, Visual Design</p>
+                    <p>
+                        Supported instructor with tests, curriculum, and
+                        assignment grading for Biomedical Programming in Python
+                        Class
+                    </p>
                 </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2006 - 2008"
-                    iconStyle={{
-                        background: "rgb(33, 150, 243)",
-                        color: "#fff",
-                    }}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Web Designer
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        San Francisco, CA
-                    </h4>
-                    <p>User Experience, Visual Design</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="April 2013"
-                    iconStyle={{
-                        background: "rgb(233, 30, 99)",
-                        color: "#fff",
-                    }}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Content Marketing for Web, Mobile and Social Media
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        Online Course
-                    </h4>
-                    <p>Strategy, Social Media</p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--education"
-                    date="November 2012"
-                    iconStyle={{
-                        background: "rgb(233, 30, 99)",
-                        color: "#fff",
-                    }}
-                >
-                    <h3 className="vertical-timeline-element-title">
-                        Agile Development Scrum Master
-                    </h3>
-                    <h4 className="vertical-timeline-element-subtitle">
-                        Certification
-                    </h4>
-                    <p>Creative Direction, User Experience, Visual Design</p>
-                </VerticalTimelineElement>
+                
+                
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
                     date="2002 - 2006"
