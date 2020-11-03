0dd1d803975c6752987279dc7106555b688f7c03
lpjune <laurenporsini@gmail.com>
2020-11-03 00:22:51 -0600

    fixed about

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,33 @@
 import React from "react";
-import { Avatar, Button, Container, Typography, Grid } from "@material-ui/core";
+import { Avatar, Button, Container, Typography } from "@material-ui/core";
 import "../App.css";
 
 const About = () => {
     return (
         <div className="about-div">
-            <Container>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <div className="about-text-container">
-                            <Typography
-                                className="about-title"
-                                variant="h4"
-                                component="h4"
-                            >
-                                About Me
-                            </Typography>
-                            <Typography variant="body1" component="p">
-                                A full stack web developer with experience in
-                                the internet industry and a B.S. in C.S. My
-                                current favorite tech stack includes ReactJS,
-                                GraphQL, & MySQL.
-                            </Typography>
-                            <Button className="about-btn" variant="contained">Download Resume</Button>
-                        </div>
-                    </Grid>
-                    <Grid item xs={6}>
-                        {/* <img
-                            className="about-image"
-                            src="/images/undraw_programmer_imem.svg"
-                            alt="Lauren Orsini"
-                        /> */}
-                        <Avatar
-                            className="about-image"
-                            src="/images/female-developer.jpg"
-                            alt="Lauren Orsini"
-                        ></Avatar>
-                    </Grid>
-                </Grid>
+            <Container className="about-container">
+                <div className="about-text-container">
+                    <Typography
+                        className="about-title"
+                        variant="h4"
+                        component="h4"
+                    >
+                        About Me
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                        A full stack web developer with experience in the
+                        internet industry and a B.S. in C.S. My current favorite
+                        tech stack includes ReactJS, GraphQL, & MySQL.
+                    </Typography>
+                    <Button className="about-btn" variant="contained">
+                        Download Resume
+                    </Button>
+                </div>
+                <Avatar
+                    className="about-image"
+                    src="/images/female-developer.jpg"
+                    alt="Lauren Orsini"
+                ></Avatar>
             </Container>
         </div>
     );
