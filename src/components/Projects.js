4cb8e2320a1acd74eeac90bbc446ade6f421725b
lpjune <laurenporsini@gmail.com>
2020-12-25 01:02:49 -0600

    playlistener demo btn

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,6 +22,7 @@ const Projects = () => {
                         text="A webapp for converting YouTube playlists to Spotify and saving to the user's account. Uses React, Express, and YouTube and Spotify's APIs."
                         imagePath="/images/playlistener.jpg"
                         githubUrl="https://github.com/lpjune/playlistener"
+                        demoUrl="https://playlistener0.herokuapp.com/"
                     />
                 </Grid>
                 <Grid
@@ -36,7 +37,7 @@ const Projects = () => {
                         text="A full-stack messenger clone built with React, GraphQL + Apollo, and MySQL. Deployed using AWS EC2 and RDS."
                         imagePath="/images/social.jpg"
                         githubUrl="https://github.com/lpjune/chat-clone"
-                        demoUrl="http://ec2-3-83-25-119.compute-1.amazonaws.com"
+                        // demoUrl="http://ec2-3-83-25-119.compute-1.amazonaws.com"
                     />
                 </Grid>
                 {/* <Grid
